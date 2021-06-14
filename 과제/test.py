19d49609de89d916bce64cf6cbb5dc5d2b4e7a4f
Minsik113 <54586341+Minsik113@users.noreply.github.com>
2021-06-14 17:51:50 +0900

    과제10과11 class diagram

--- 과제/test.py
+++ 과제/test.py
@@ -1,34 +1,58 @@
-import random
 from abc import ABCMeta
 from abc import abstractmethod
-import turtle
+import turtle, random
 
-#Parent
 class Duck(metaclass=ABCMeta):
     
     SIZE = 20
-    color_list = ['blue', 'red', 'orange', 'green']
-    
+    color_list = ['blue', 'red', 'yellow', 'green']
+
     def __init__(self):
         self._x = random.randint(-300, 300)
         self._y = random.randint(-300, 300)
-        self._color = -1
+        self._flycheck = False
+        self._quackcheck = False
         self.turtle = turtle
-    
+
     @abstractmethod
     def display(self):
         pass
 
-    @abstractmethod
     def swim(self):
+        self.turtle.penup()
+        self.turtle.goto(self._x - 50, self,_y + 30)
+        self.turtle.write("수영")
+        self.turtle.pendown()
+
+class Quack(metaclass=ABCMeta):
+    @abstractmethod
+    def quack(self):
         pass
 
-#Child1
-class MallardDuck(Duck):
+class Fly(metaclass=ABCMeta):
+    @abstractmethod
+    def fly(self):
+        pass
+
+class MallardDuck(Duck, Quack, Fly):
     
     def __init__(self):
         super(MallardDuck, self).__init__()
         self._color = 0
+        self._flycheck = True
+        self._quackcheck = True
+    
+    #--abstract method--
+    def display(self):
+        self.turtle.color(Duck.color_list[self._color])
+        
+        self.turtle.penup()
+        self.goto(self._x, self._y)
+        self.turtle.pendown()
+
+        self.turtle.begin_fill()
+        self.turtle.circle(self.SIZE)
+        self.turtle.end_fill()
     
     def quack(self):
         self.turtle.penup()
@@ -41,36 +65,24 @@ class MallardDuck(Duck):
         self.turtle.goto(self._x - 50, self._y - 20)
         self.turtle.write("날다")
         self.turtle.pendown()
-        
-    #--abstract method--
-    def display(self):
-        print("BLUE")
-        self.turtle.color(Duck.color_list[self._color])
-        
-        self.turtle.penup()
-        self.turtle.goto(self._x, self._y)
-        self.turtle.pendown()
-        
-        self.turtle.begin_fill()
-        self.turtle.circle(self.SIZE)
-        self.turtle.end_fill()
-    
-    def swim(self):
-        self.turtle.penup()
-        self.turtle.goto(self._x - 50, self._y + 30)
-        self.turtle.write("수영")
-        self.turtle.pendown()
 
-for i in range(2):
-    ml = MallardDuck()
-    if ml != None:
-        ml.display()
-        ml.swim()
-        ml.quack()
-        ml.fly()
+class RedDuck(Duck, Quack, Fly):
+    pass
+class RubberDuck(Duck, Quack):
+    pass
+class DecoyDuck(Duck):
+    pass
 
-# ml = MallardDuck()
-# ml.display()
-# ml.swim()
-# ml.quack()
-# ml.fly()
+class DuckManager:
+    __duck_list = []
+
+    def __init__(self):
+        self.createDucks()
+
+    def createDucks(self):
+        pass
+    def displayAllDucks(self):
+        pass
+
+if __name__ == "__main__":
+    dm = DuckManager()
